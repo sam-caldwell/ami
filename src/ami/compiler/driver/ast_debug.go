a4b89358c8893b0474bd489a331e5112dd395c9d
Sam Caldwell <github@samcaldwell.net>
2025-09-27 07:25:57 -0500

    working toward MVP

--- src/ami/compiler/driver/ast_debug.go
+++ src/ami/compiler/driver/ast_debug.go
@@ -7,6 +7,7 @@ import (
     "sort"
 
     "github.com/sam-caldwell/ami/src/ami/compiler/ast"
+    "github.com/sam-caldwell/ami/src/ami/compiler/source"
 )
 
 type astUnit struct {
@@ -22,6 +23,7 @@ type astUnit struct {
 type astImport struct {
     Path       string `json:"path"`
     Constraint string `json:"constraint,omitempty"`
+    Pos        *dbgPos `json:"pos,omitempty"`
 }
 
 type astTypeParam struct {
@@ -35,17 +37,21 @@ type astFunc struct {
     Params     []string       `json:"params,omitempty"`
     Results    []string       `json:"results,omitempty"`
     Decorators []astDecorator `json:"decorators,omitempty"`
+    Pos        *dbgPos        `json:"pos,omitempty"`
+    NamePos    *dbgPos        `json:"namePos,omitempty"`
 }
 
 type astPipe struct {
     Name  string       `json:"name"`
     Steps []astPipeStep `json:"steps"`
+    Pos   *dbgPos      `json:"pos,omitempty"`
 }
 
 type astPipeStep struct {
     Name string   `json:"name"`
     Args []string `json:"args,omitempty"`
     Attrs []astAttr `json:"attrs,omitempty"`
+    Pos  *dbgPos  `json:"pos,omitempty"`
 }
 
 type astAttr struct {
@@ -56,6 +62,7 @@ type astAttr struct {
 type astDecorator struct {
     Name string   `json:"name"`
     Args []string `json:"args,omitempty"`
+    Pos  *dbgPos  `json:"pos,omitempty"`
 }
 
 type astPragma struct {
@@ -64,6 +71,18 @@ type astPragma struct {
     Value  string            `json:"value,omitempty"`
     Args   []string          `json:"args,omitempty"`
     Params map[string]string `json:"params,omitempty"`
+    Pos    *dbgPos           `json:"pos,omitempty"`
+}
+
+type dbgPos struct {
+    Line   int `json:"line"`
+    Column int `json:"column"`
+    Offset int `json:"offset"`
+}
+
+func posPtr(p source.Position) *dbgPos {
+    if p.Line == 0 && p.Column == 0 && p.Offset == 0 { return nil }
+    return &dbgPos{Line: p.Line, Column: p.Column, Offset: p.Offset}
 }
 
 // writeASTDebug writes a per-unit AST summary as ast.v1 JSON.
@@ -75,7 +94,7 @@ func writeASTDebug(pkg, unit string, f *ast.File) (string, error) {
     for _, d := range f.Decls {
         switch n := d.(type) {
         case *ast.ImportDecl:
-            u.Imports = append(u.Imports, astImport{Path: n.Path, Constraint: n.Constraint})
+            u.Imports = append(u.Imports, astImport{Path: n.Path, Constraint: n.Constraint, Pos: posPtr(n.Pos)})
         case *ast.FuncDecl:
             var tf []astTypeParam
             for _, tp := range n.TypeParams { tf = append(tf, astTypeParam{Name: tp.Name, Constraint: tp.Constraint}) }
@@ -89,7 +108,7 @@ func writeASTDebug(pkg, unit string, f *ast.File) (string, error) {
                 for _, e := range d.Args { a = append(a, decoExprText(e)) }
                 decos = append(decos, astDecorator{Name: d.Name, Args: a})
             }
-            u.Funcs = append(u.Funcs, astFunc{Name: n.Name, TypeParams: tf, Params: ps, Results: rs, Decorators: decos})
+            u.Funcs = append(u.Funcs, astFunc{Name: n.Name, TypeParams: tf, Params: ps, Results: rs, Decorators: decos, Pos: posPtr(n.Pos), NamePos: posPtr(n.NamePos)})
         case *ast.PipelineDecl:
             var steps []astPipeStep
             for _, s := range n.Stmts {
@@ -103,15 +122,15 @@ func writeASTDebug(pkg, unit string, f *ast.File) (string, error) {
                         for _, aa := range at.Args { aargs = append(aargs, aa.Text) }
                         attrs = append(attrs, astAttr{Name: at.Name, Args: aargs})
                     }
-                    steps = append(steps, astPipeStep{Name: st.Name, Args: args, Attrs: attrs})
+                    steps = append(steps, astPipeStep{Name: st.Name, Args: args, Attrs: attrs, Pos: posPtr(st.Pos)})
                 }
             }
-            u.Pipelines = append(u.Pipelines, astPipe{Name: n.Name, Steps: steps})
+            u.Pipelines = append(u.Pipelines, astPipe{Name: n.Name, Steps: steps, Pos: posPtr(n.Pos)})
         }
     }
     // pragmas
     for _, pr := range f.Pragmas {
-        ap := astPragma{Domain: pr.Domain, Key: pr.Key, Value: pr.Value}
+        ap := astPragma{Domain: pr.Domain, Key: pr.Key, Value: pr.Value, Pos: posPtr(pr.Pos)}
         if len(pr.Args) > 0 { ap.Args = append(ap.Args, pr.Args...) }
         if len(pr.Params) > 0 { ap.Params = pr.Params }
         u.Pragmas = append(u.Pragmas, ap)

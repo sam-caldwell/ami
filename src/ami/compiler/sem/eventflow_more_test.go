7cb305ea0c36e42c100644a8aa19d9f67442f005
Sam Caldwell <github@samcaldwell.net>
2025-09-27 12:25:08 -0500

    working toward MVP

---
+++ src/ami/compiler/sem/eventflow_more_test.go
@@ -0,0 +1,28 @@
+package sem
+
+import (
+    "testing"
+    "github.com/sam-caldwell/ami/src/ami/compiler/parser"
+    "github.com/sam-caldwell/ami/src/ami/compiler/source"
+)
+
+func TestAnalyzeEventTypeFlow_MismatchAcrossEdge(t *testing.T) {
+    src := "package app\npipeline P(){ Alpha type(\"int\") -> Beta type(\"string\"); egress }\n"
+    f := &source.File{Name: "ev1.ami", Content: src}
+    p := parser.New(f)
+    af, _ := p.ParseFile()
+    ds := AnalyzeEventTypeFlow(af)
+    found := false
+    for _, d := range ds { if d.Code == "E_EVENT_TYPE_FLOW" { found = true } }
+    if !found { t.Fatalf("expected E_EVENT_TYPE_FLOW: %+v", ds) }
+}
+
+func TestAnalyzeEventTypeFlow_MultiUpstreamMismatch(t *testing.T) {
+    src := "package app\npipeline P(){ A type(\"int\"); B type(\"string\"); A -> Collect; B -> Collect; Collect; egress }\n"
+    f := &source.File{Name: "ev2.ami", Content: src}
+    p := parser.New(f)
+    af, _ := p.ParseFile()
+    ds := AnalyzeEventTypeFlow(af)
+    if len(ds) == 0 { t.Fatalf("expected mismatch on collect") }
+}
+

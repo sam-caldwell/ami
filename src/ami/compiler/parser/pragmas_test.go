0e70776778dbfc319b7cf5bcbc3f7d9e06bf2e73
Sam Caldwell <github@samcaldwell.net>
2025-09-27 12:21:04 -0500

    working toward MVP

---
+++ src/ami/compiler/parser/pragmas_test.go
@@ -0,0 +1,14 @@
+package parser
+
+import (
+    "testing"
+    "github.com/sam-caldwell/ami/src/ami/compiler/source"
+)
+
+func TestParser_PragmaCollection(t *testing.T) {
+    src := "package app\n#pragma test:case name a=1 b=\"y\"\n"
+    f := (&source.FileSet{}).AddFile("pr.ami", src)
+    p := New(f)
+    file, _ := p.ParseFile()
+    if len(file.Pragmas) == 0 { t.Fatalf("expected pragmas collected") }
+}

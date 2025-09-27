610d01b19f8884d4e63c26c41c6e86b9df8a7cb9
Sam Caldwell <github@samcaldwell.net>
2025-09-27 07:01:28 -0500

    working toward MVP

--- src/ami/compiler/parser/decorators_test.go
+++ src/ami/compiler/parser/decorators_test.go
@@ -8,7 +8,8 @@ import (
 
 func TestParse_Decorators_Single_NoArgs(t *testing.T) {
     src := "package p\n@trace\nfunc F() {}\n"
-    f := source.NewFile("f.ami", src)
+    var fs source.FileSet
+    f := fs.AddFile("f.ami", src)
     p := New(f)
     file, err := p.ParseFile()
     if err != nil { t.Fatalf("parse error: %v", err) }
@@ -21,7 +22,8 @@ func TestParse_Decorators_Single_NoArgs(t *testing.T) {
 
 func TestParse_Decorators_Multiple_WithArgs_OrderPreserved(t *testing.T) {
     src := "package p\n@deprecated(\"x\")\n@metrics(1, id)\nfunc F() {}\n"
-    f := source.NewFile("f.ami", src)
+    var fs source.FileSet
+    f := fs.AddFile("f.ami", src)
     p := New(f)
     file, err := p.ParseFile()
     if err != nil { t.Fatalf("parse error: %v", err) }
@@ -31,4 +33,13 @@ func TestParse_Decorators_Multiple_WithArgs_OrderPreserved(t *testing.T) {
     if got := len(fn.Decorators); got != 2 { t.Fatalf("decorators: %d", got) }
     if fn.Decorators[0].Name != "deprecated" { t.Fatalf("first: %s", fn.Decorators[0].Name) }
     if fn.Decorators[1].Name != "metrics" { t.Fatalf("second: %s", fn.Decorators[1].Name) }
+    if len(fn.Decorators[1].Args) != 2 { t.Fatalf("metrics args: %d", len(fn.Decorators[1].Args)) }
+    // first arg should be number literal "1"
+    if num, ok := fn.Decorators[1].Args[0].(*ast.NumberLit); !ok || num.Text != "1" {
+        t.Fatalf("arg0 not number '1': %#v", fn.Decorators[1].Args[0])
+    }
+    // second arg should be identifier 'id'
+    if id, ok := fn.Decorators[1].Args[1].(*ast.IdentExpr); !ok || id.Name != "id" {
+        t.Fatalf("arg1 not ident 'id': %#v", fn.Decorators[1].Args[1])
+    }
 }
